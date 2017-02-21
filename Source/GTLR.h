ac7fd32333ba8d0f36fa183304bf4e794df7ba49
Rupert <rupert@pearlshare.com>
2017-02-21 15:50:36 +0100

    Added umbrella header.

---
+++ Source/GTLR.h
@@ -0,0 +1,26 @@
+//
+//  GTLR.h
+//  GTLRCore
+//
+//  Created by Rupert on 21/02/2017.
+//
+//
+
+#ifndef GTLR_h
+#define GTLR_h
+
+
+#endif /* GTLR_h */
+
+
+#import <GTLR/GTLRFramework.h>
+#import <GTLR/GTLRBatchQuery.h>
+#import <GTLR/GTLRBatchResult.h>
+#import <GTLR/GTLRDateTime.h>
+#import <GTLR/GTLRDuration.h>
+#import <GTLR/GTLRErrorObject.h>
+#import <GTLR/GTLRObject.h>
+#import <GTLR/GTLRQuery.h>
+#import <GTLR/GTLRRuntimeCommon.h>
+#import <GTLR/GTLRService.h>
+#import <GTLR/GTLRUploadParameters.h>
